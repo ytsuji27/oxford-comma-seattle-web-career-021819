4cbff5d40e79c5a0fe2868a54bb4390d009dbeab
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-13 05:23:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,11 @@ def oxford_comma(array)
     array.join
   elsif array.count == 2
     array.join(" and ")
-  else
-    while i < array.count-2
-      string.concat("#{array[i], "")
-      i += 1
-    end
-    string.concat("#{array[array.count-2]} and #{array[array.count-1]}")
+#  else
+#    while i < array.count-2
+#      string.concat("#{array[i], "")
+#      i += 1
+#    end
+#    string.concat("#{array[array.count-2]} and #{array[array.count-1]}")
   end
 end
