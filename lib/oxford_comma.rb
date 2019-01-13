69547677c4e89f43cfbc3f5636be0441dc17e936
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-13 05:41:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  i = 0
+  string = ""
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    while i < array.count-1
+      string << "#{array[i]}, "
+      i += 1
+    end
+    string << "and #{array[array.count-1]}"
+  end
 end
