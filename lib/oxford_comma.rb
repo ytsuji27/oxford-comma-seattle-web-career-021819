3e4255b3b34b222cc2ed85f10e5daf8393aafa56
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-13 05:14:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,5 @@ def oxford_comma(array)
   while i < array.count-1
     string.concat("#{array[i], "")
   end
-  string.concat("#{array[array.count-2]} and #{array[array.count-1]}")
+#  string.concat("#{array[array.count-2]} and #{array[array.count-1]}")
 end
